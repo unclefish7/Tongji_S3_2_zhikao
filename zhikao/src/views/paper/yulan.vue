06f0b6567e0d4cb68497c65881b5d34f08c71579
abcd23456 <144504704+abcd23456@users.noreply.github.com>
2025-04-16 17:09:50 +0800

    添加合并功能

--- zhikao/src/views/paper/yulan.vue
+++ zhikao/src/views/paper/yulan.vue
@@ -1,11 +1,8 @@
 <template>
   <el-main>
-
     <div class="button-container">
       <el-button type="primary" size="small" @click="backPage()">返回</el-button>
-
     </div>
-
   </el-main>
 </template>
 
@@ -15,36 +12,32 @@ export default {
     return {
       paperId: 0,
     }
-    
   },
   methods: {
-
-backPage(){
-  this.$router.back();
-},
-
-
-
-}
+    backPage(){
+      this.$router.back();
+    },
+  }
 }
 </script>
-<style lang="scss" scoped>
-.el-main{
-  padding-top: 5px !important;
-}
-.aa{
-  width: 300px;
-  height: 28px;
-  margin-bottom: 35px;
-}
 
-.button-container {
-  position: relative;
-}
-.button-container span {
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  color: #333;
-}
+<style lang="scss" scoped>
+  .el-main{
+    padding-top: 5px !important;
+  }
+  .aa{
+    width: 300px;
+    height: 28px;
+    margin-bottom: 35px;
+  }
+  .button-container {
+    position: relative;
+  }
+  .button-container span {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #333;
+  }
+</style>
